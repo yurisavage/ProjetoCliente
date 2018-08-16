f782c5d014ad903e4f3c17e017eb9aaa6424233c
yurisavage <yuritec@gmail.com>
2018-08-16 09:54:00 -0300

    Add files via upload

---
+++ Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entities
+{
+    public class Cliente
+    {
+        public virtual int IdCliente { get; set; }
+        public virtual string Nome { get; set; }
+        public virtual string Email { get; set; }
+        public virtual DateTime DataCadastro { get; set; }
+
+    }
+}
